9f2552b0ca73db0af75f278294cf63cac4fdc1e9
Sagor Sarker <brursagor@gmail.com>
2017-06-30 18:12:55 +0600

    first commit

---
+++ CalculatorAPI/src/sagor/calculator/calculatorAPI/Calculator.java
@@ -0,0 +1,26 @@
+package sagor.calculator.calculatorAPI;
+
+public class Calculator {
+	
+	public static double add(double a,double b)
+	{
+		return a+b;
+	}
+	public static double sub(double a,double b)
+	{
+		return a-b;
+	}
+	public static double mul(double a,double b)
+	{
+		return a*b;
+	}
+	public static double div(double a,double b)
+	{
+		return a/b;
+	}
+	public static double rem(double a,double b){
+		return a%b;
+	}
+	
+}
+
